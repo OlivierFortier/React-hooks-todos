1a96d2b438dfca8b8772a45a013f3a59770bf713
OlivierFortier <olivier_fortier@outlook.com>
2020-01-11 13:01:48 -0500

    removed unused stuff and cleaned files

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,6 +3,6 @@ import Todo from "./Todo";
 
 export default function TodoList({ todos, toggleTodo }) {
   return todos.map(todo => {
-    return <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo}/>;
+    return <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />;
   });
 }

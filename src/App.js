bb83051209dc3bc1a144d4d0e9caa232ea9f9fb8
OlivierFortier <olivier_fortier@outlook.com>
2020-01-11 12:53:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import TodoList from "./TodoList";
+import uuidv4 from "uuid/v4";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    const id = uuidv4();
+    if (name === "") return;
+    setTodos(prevTodo => {
+      return [...prevTodo, { id: id, name: name, completed: false }];
+    });
+    todoNameRef.current.value = "";
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.completed);
+    setTodos(newTodos);
+  }
+
+  function handleToggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} toggleTodo={handleToggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Todos</button>
+      <div>{todos.filter(todo => !todo.completed).length} left to do</div>
+    </>
   );
 }
 
